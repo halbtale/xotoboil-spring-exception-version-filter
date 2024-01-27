160ac2a63149aa5716f42fe17e076c3034bd4132
Alberto <42894888+Nenino2@users.noreply.github.com>
2024-01-27 17:46:35 +0100

    first commit

---
+++ src/test/java/com/xotoboil/springboot/restapiboilerplate/RestApiBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xotoboil.springboot.restapiboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
