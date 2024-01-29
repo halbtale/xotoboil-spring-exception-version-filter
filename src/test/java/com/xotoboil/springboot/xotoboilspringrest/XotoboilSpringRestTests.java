152c8a4a5750e259ea70ef408ca26dcc8aefb9e1
Alberto <42894888+Nenino2@users.noreply.github.com>
2024-01-29 14:14:12 +0100

    Refactor name tests

--- src/test/java/com/xotoboil/springboot/xotoboilspringrest/XotoboilSpringRestTests.java
+++ src/test/java/com/xotoboil/springboot/xotoboilspringrest/XotoboilSpringRestTests.java
@@ -1,10 +1,10 @@
-package com.xotoboil.springboot.restapiboilerplate;
+package com.xotoboil.springboot.xotoboilspringrest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestApiBoilerplateApplicationTests {
+class XotoboilSpringRestTests {
 
 	@Test
 	void contextLoads() {
